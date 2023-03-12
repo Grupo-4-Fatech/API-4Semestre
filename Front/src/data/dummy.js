11b6f373f1a7a1f8fb48ca45d092ec68507f1954
Antonio Marcelo <antoniomarcelo545@gmail.com>
2023-03-11 21:18:50 -0300

    Front

---
+++ Front/src/data/dummy.js
@@ -0,0 +1,340 @@
+import React from 'react';
+import {  FiEdit } from 'react-icons/fi';
+import { BsKanban,BsCurrencyDollar} from 'react-icons/bs';
+import avatar from './avatar.jpg';
+import avatar2 from './avatar2.jpg';
+
+
+export const gridOrderImage = (props) => (
+  <div>
+    <img
+      className="rounded-xl h-20 md:ml-3"
+      src={props.ProductImage}
+      alt="order-item"
+    />
+  </div>
+);
+
+export const gridOrderStatus = (props) => (
+  <button
+    type="button"
+    style={{ background: props.StatusBg }}
+    className="text-white py-1 px-2 capitalize rounded-2xl text-md"
+  >
+    {props.Status}
+  </button>
+);
+
+export const kanbanGrid = [
+  { headerText: 'To Do',
+    keyField: 'Open',
+    allowToggle: true },
+
+  { headerText: 'In Progress',
+    keyField: 'InProgress',
+    allowToggle: true },
+
+  { headerText: 'Testing',
+    keyField: 'Testing',
+    allowToggle: true,
+    isExpanded: false },
+
+  { headerText: 'Done',
+    keyField: 'Close',
+    allowToggle: true },
+];
+
+const customerGridImage = (props) => (
+  <div className="image flex gap-4">
+    <img
+      className="rounded-full w-10 h-10"
+      src={props.CustomerImage}
+      alt="employee"
+    />
+    <div>
+      <p>{props.CustomerName}</p>
+      <p>{props.CustomerEmail}</p>
+    </div>
+  </div>
+);
+
+const customerGridStatus = (props) => (
+  <div className="flex gap-2 justify-center items-center text-gray-700 capitalize">
+    <p style={{ background: props.StatusBg }} className="rounded-full h-3 w-3" />
+    <p>{props.Status}</p>
+  </div>
+);
+
+
+
+
+
+
+
+
+
+
+
+export const customersGrid = [
+  { type: 'checkbox', width: '50' },
+  { headerText: 'Name',
+    width: '150',
+    template: customerGridImage,
+    textAlign: 'Center' },
+  { field: 'ProjectName',
+    headerText: 'Project Name',
+    width: '150',
+    textAlign: 'Center' },
+  { field: 'Status',
+    headerText: 'Status',
+    width: '130',
+    format: 'yMd',
+    textAlign: 'Center',
+    template: customerGridStatus },
+  {
+    field: 'Weeks',
+    headerText: 'Weeks',
+    width: '100',
+    format: 'C2',
+    textAlign: 'Center' },
+  { field: 'Budget',
+    headerText: 'Budget',
+    width: '100',
+    format: 'yMd',
+    textAlign: 'Center' },
+
+  { field: 'Location',
+    headerText: 'Location',
+    width: '150',
+    textAlign: 'Center' },
+
+  { field: 'CustomerID',
+    headerText: 'Customer ID',
+    width: '120',
+    textAlign: 'Center',
+    isPrimaryKey: true,
+  },
+
+];
+
+
+
+// SideBar 
+export const links = [ 
+  {
+    title: 'Pages',
+    links: [
+      {
+        name: 'kanban',
+        icon: <BsKanban />,
+      },
+      {
+        name: 'Open hotFlix',
+        icon: <FiEdit />,
+      },
+      {
+        name: 'Open new feature',
+        icon: <FiEdit />,
+      },
+    ],
+  }
+  
+];
+
+export const themeColors = [
+  {
+    name: 'blue-theme',
+    color: '#1A97F5',
+  },
+  {
+    name: 'green-theme',
+    color: '#03C9D7',
+  },
+  {
+    name: 'purple-theme',
+    color: '#7352FF',
+  },
+  {
+    name: 'red-theme',
+    color: '#FF5C8E',
+  },
+  {
+    name: 'indigo-theme',
+    color: '#1E4DB7',
+  },
+  {
+    color: '#FB9678',
+    name: 'orange-theme',
+  },
+];
+
+export const userProfileData = [
+  {
+    icon: <BsCurrencyDollar />,
+    title: 'My Profile',
+    desc: 'Account Settings',
+    iconColor: '#03C9D7',
+    iconBg: '#E5FAFB',
+  },
+];
+
+
+export const customersData = [ 
+  {
+    EmployeeID: 4,
+    Name: 'Penjani Inyene',
+    Title: 'Marketing Head',
+    HireDate: '01/02/2021',
+    Country: 'USA',
+    ReportsTo: 'Carson',
+    EmployeeImage:
+      avatar,
+  },
+  {
+    EmployeeID: 5,
+    Name: 'Miron Vitold',
+    Title: 'HR',
+    HireDate: '01/02/2021',
+    Country: 'USA',
+    ReportsTo: 'Carson',
+    EmployeeImage:
+      avatar2,
+  },
+];
+
+export const contextMenuItems = [
+  'AutoFit',
+  'AutoFitAll',
+  'SortAscending',
+  'SortDescending',
+  'Copy',
+  'Edit',
+  'Delete',
+  'Save',
+  'Cancel',
+  'PdfExport',
+  'ExcelExport',
+  'CsvExport',
+  'FirstPage',
+  'PrevPage',
+  'LastPage',
+  'NextPage',
+];
+export const kanbanData = [
+ 
+  
+  {
+    Id: 'Task 3',
+    Title: 'Task - 29003',
+    Status: 'Open',
+    Summary: 'Arrange a web meeting with the customer to get new requirements.',
+    Type: 'Others',
+    Priority: 'Critical',
+    Tags: 'Meeting',
+    Estimate: 5.5,
+    Assignee: 'Janet Leverling',
+    RankId: 2,
+    Color: '#1F88E5',
+    ClassName: 'e-others, e-critical, e-janet-leverling',
+  },
+  {
+    Id: 'Task 4',
+    Title: 'Task - 29004',
+    Status: 'InProgress',
+    Summary: 'Fix the issues reported in the IE browser.',
+    Type: 'Bug',
+    Priority: 'Critical',
+    Tags: 'IE',
+    Estimate: 2.5,
+    Assignee: 'Janet Leverling',
+    RankId: 2,
+    Color: '#E64A19',
+    ClassName: 'e-bug, e-release, e-janet-leverling',
+  },
+  {
+    Id: 'Task 5',
+    Title: 'Task - 29005',
+    Status: 'Review',
+    Summary: 'Fix the issues reported by the customer.',
+    Type: 'Bug',
+    Priority: 'Low',
+    Tags: 'Customer',
+    Estimate: '3.5',
+    Assignee: 'Steven walker',
+    RankId: 1,
+    Color: '#E64A19',
+    ClassName: 'e-bug, e-low, e-steven-walker',
+  },
+  {
+    Id: 'Task 6',
+    Title: 'Task - 29007',
+    Status: 'Validate',
+    Summary: 'Validate new requirements',
+    Type: 'Improvement',
+    Priority: 'Low',
+    Tags: 'Validation',
+    Estimate: 1.5,
+    Assignee: 'Robert King',
+    RankId: 1,
+    Color: '#673AB8',
+    ClassName: 'e-improvement, e-low, e-robert-king',
+  },
+  {
+    Id: 'Task 7',
+    Title: 'Task - 29009',
+    Status: 'Review',
+    Summary: 'Fix the issues reported in Safari browser.',
+    Type: 'Bug',
+    Priority: 'Critical',
+    Tags: 'Fix,Safari',
+    Estimate: 1.5,
+    Assignee: 'Nancy Davloio',
+    RankId: 2,
+    Color: '#E64A19',
+    ClassName: 'e-bug, e-release, e-nancy-davloio',
+  },
+  {
+    Id: 'Task 8',
+    Title: 'Task - 29010',
+    Status: 'Close',
+    Summary: 'Test the application in the IE browser.',
+    Type: 'Story',
+    Priority: 'Low',
+    Tags: 'Review,IE',
+    Estimate: 5.5,
+    Assignee: 'Margaret hamilt',
+    RankId: 3,
+    Color: '#02897B',
+    ClassName: 'e-story, e-low, e-margaret-hamilt',
+  },
+  {
+    Id: 'Task 9',
+    Title: 'Task - 29011',
+    Status: 'Validate',
+    Summary: 'Validate the issues reported by the customer.',
+    Type: 'Story',
+    Priority: 'High',
+    Tags: 'Validation,Fix',
+    Estimate: 1,
+    Assignee: 'Steven walker',
+    RankId: 1,
+    Color: '#02897B',
+    ClassName: 'e-story, e-high, e-steven-walker',
+  },
+  {
+    Id: 'Task 10',
+    Title: 'Task - 29015',
+    Status: 'Open',
+    Summary: 'Show the retrieved data from the server in grid control.',
+    Type: 'Story',
+    Priority: 'High',
+    Tags: 'Database,SQL',
+    Estimate: 5.5,
+    Assignee: 'Margaret hamilt',
+    RankId: 4,
+    Color: '#02897B',
+    ClassName: 'e-story, e-high, e-margaret-hamilt',
+  }
+];
+
+
---
+++ Front/src/pages/index.jsx
@@ -0,0 +1,4 @@
+
+
+export { default as Customers } from './Customers';
+export { default as Kanban } from './Kanban';
