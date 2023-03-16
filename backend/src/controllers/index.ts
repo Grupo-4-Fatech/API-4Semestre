e828d8977b9a8e36faf7ab1f91ec25553ba72b8b
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2023-03-16 10:59:06 -0300

    back

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,11 @@
+import Data  from "./Data";
+import Login from "./Login";
+
+import UserController from "./UserController";
+
+export{
+    Data,
+    
+    Login,
+    UserController
+};
---
+++ backend/src/routes/user.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+// import {Data} from "../controllers";
+import { UserController } from "../controllers";
+const routes = Router();
+
+
+routes.post("/create", UserController.create);
+// routes.get('/day', Data.dia);
+// routes.get('/month', Data.mes);
+// routes.get('/year', Data.ano);
+
+export default routes;
