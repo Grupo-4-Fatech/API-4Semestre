8c2a574e32bc02aaef17a5388ee4443093fe68ee
Bruna <brunadias_30@hotmail.com>
2023-03-18 19:13:40 -0300

    Fix - Link Footer according to current color

--- Front/src/components/Footer.jsx
+++ Front/src/components/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useStateContext } from '../contexts/ContextProvider';
 
-const Footer = () => (
+const Footer = () => {
+  const { currentColor } = useStateContext();
+  return (
   <div className="mt-24">
     <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-      © 2023 All rights reserved by https://pt-br.ionic.health
+      © 2023 All rights reserved by
+      <a 
+        className='font-medium hover:underline' 
+        href="https://pt-br.ionic.health"
+        style={{color: currentColor}}> Ionic Health</a> 
     </p>
   </div>
-);
+)};
 
 export default Footer;
