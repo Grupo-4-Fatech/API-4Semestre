17071aee34af90e585b24621db5ec737e6eaaa3f
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-03-26 02:48:23 -0300

    criação validações

--- Front/src/components/Descrition.jsx
+++ Front/src/components/Descrition.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import tinymce from 'tinymce'
 import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
 
-export default function Descrition({value,setValue}) {
-    var rteObject  = typeof RichTextEditorComponent;
+export default function Descrition({ value, setValue }) {
+    
+    var rteObject = typeof RichTextEditorComponent;
     return (
         <>
-        <div className='pt-6'>
-        <label className="text-lg font-bold dark:text-black">Description</label>
-            <RichTextEditorComponent placeholder={'Write the description'} ref={(richEditor)=>{rteObject=richEditor}} onBlur={(e)=>{ setValue(rteObject.getHtml())}}  value={value} >
-                <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
-            </RichTextEditorComponent>
+            <div className='pt-6'>
+                <label className="text-lg font-bold dark:text-black">Description</label>
+                <RichTextEditorComponent id='teste' placeholder={'Write the description'} ref={(richEditor) => { rteObject = richEditor }} onBlur={(e) => { setValue(rteObject.getHtml()) }} value={value} >
+                    <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar,]} />
+                </RichTextEditorComponent>
             </div>
         </>
 
--- Front/src/pages/ViewTicket.jsx
+++ Front/src/pages/ViewTicket.jsx
@@ -1,57 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useStateContext } from '../contexts/ContextProvider'
+const Swal = require('sweetalert2')
+
 
 const ViewTicket = () => {
     const { currentColor } = useStateContext();
-    const headers = ['Title', 'Classification', 'Status', 'Edit', 'File']
-    const data = [
-        {title:'Conexão banco de dados',classification: 'Hotfix',status:'Em fila'},
-        {title:'Página de suporte', classification: 'Feature', status:'Em andamento'},
-        {title:'Puxando Id Ticket', classification: 'Feature', status:'Arquivado'},
-        {title:'Criação Ticket', classification: 'Hotfix', status:'Aprovado'}
-    ] 
-    return (
+
+    const headers = ['Title', 'Classification', 'Status', 'Edit', 'Archive', 'Approved']
+    const [data, setData] = useState([
+        { title: 'Conexão banco de dados', classification: 'Hotfix', status: "status" },
+        { title: 'Página de suporte', classification: 'Feature', status: 'Em andamento' },
+        { title: 'Puxando Id Ticket', classification: 'Feature', status: 'Arquivado' },
+        { title: 'Criação Ticket', classification: 'Hotfix', status: 'Aprovado' },
+    ])
+
+    const Aproved = (title, newStatus) => {
+        const updateData = data.map(item => {
+            if (item.title === title) {
+                return {...item, status: newStatus='Approved'};
+        }else {
+            Swal.fire({
+                icon: 'success',
+                title: 'Approved with success',
+            })
+            return item
+        }
+        })
+        setData(updateData)
+
+}
+const Archive = (title, newStatus) => {
+    const updateData = data.map(item => {
+        if (item.title === title) {
+            return {...item, status: newStatus='Archived'};
+            
+    }else {
+        Swal.fire({
+            icon: 'success',
+            title: 'Archived with success'
+        })
+        return item
+    }
+    })
+    setData(updateData)
+
+}
+
+return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:text-gray-400 content-center">
-            <tr>
-                {headers.map((header) => 
-                    <th scope="col" class="px-6 py-3"> 
-                        {header}
-                    </th>)}
-            </tr>
-        </thead>
-        <tbody>
-            {data.map((dat) =>
-                <tr class="bg-white hover:bg-gray-50 dark:hover:bg-gray-300 content-center">
-                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
-                        {dat.title}
-                    </td>
-                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
-                        {dat.classification}
-                    </td>
-                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
-                        {dat.status}
-                    </td>
-                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
-                        <button style={{ backgroundColor: currentColor }} className="text-white font-bold py-2 px-4 rounded inline-flex items-center right-20">Edit</button>
-                    </td>
-                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
-                    <button className="bg-red-600 text-white font-bold py-2 px-4 rounded inline-flex items-center right-20">File</button>
-                    </td>
-                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
-                    </td>
-                    
-                </tr>
-                )}
-        </tbody>
-    </table>
-</div>
+
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:text-gray-400 content-center">
+                    <tr>
+                        {headers.map((header) =>
+                            <th scope="col" class="px-6 py-3">
+                                {header}
+                            </th>)}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((dat) =>
+                        <tr class="bg-white hover:bg-gray-50 dark:hover:bg-gray-300 content-center">
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
+                                {dat.title}
+                            </td>
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
+                                {dat.classification}
+                            </td>
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
+                                {dat.status}
+                            </td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
+                                <button style={{ backgroundColor: currentColor }} className="text-white font-bold py-2 px-4 rounded inline-flex items-center right-20">Edit</button>
+                            </td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
+                                <button onClick ={(e)=> Archive(dat.title)} className="bg-red-600 text-white font-bold py-2 px-4 rounded inline-flex items-center right-20">Archive</button>
+                            </td>
+                            <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
+                                <button onClick={(e)=> Aproved(dat.title)} className="bg-green-500 text-white font-bold py-2 px-4 rounded inline-flex items-center right-20">Aproved</button>
+                            </td>
+                            <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray">
+                            </td>
+
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
 
     </div>
-  );
+);
 };
 
 export default ViewTicket;
