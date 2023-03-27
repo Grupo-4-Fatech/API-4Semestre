09e7d609ec5a9a99279b0c3bf504dc375a080a63
evertonrwr <evertonrwrocha@gmail.com>
2023-03-27 08:33:51 -0300

    fix image settings

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,12 +1,13 @@
 import * as express from "express";
 import * as dotenv from "dotenv";
 import routes from "./routes";
-
+const bodyParser = require( "body-parser");
 dotenv.config();
 
 
 const app = express();
-
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json()); 
 
 const PORT = process.env.PORT || 3000;
--- Front/src/components/Descrition.jsx
+++ Front/src/components/Descrition.jsx
@@ -9,7 +9,7 @@ export default function Descrition({ value, setValue }) {
         <>
             <div className='pt-6'>
                 <label className="text-lg font-bold dark:text-black">Description</label>
-                <RichTextEditorComponent id='teste' placeholder={'Write the description'} ref={(richEditor) => { rteObject = richEditor }} onBlur={(e) => { setValue(rteObject.getHtml()) }} value={value} >
+                <RichTextEditorComponent id='teste' placeholder={'Write the description'} ref={(richEditor) => { rteObject = richEditor }} onBlur={(e) => { setValue(rteObject.getHtml()) }} value={value} insertImageSettings={{saveFormat:"Base64"}}>
                     <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar,]} />
                 </RichTextEditorComponent>
             </div>
