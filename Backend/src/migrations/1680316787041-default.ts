df0a2bd22a0b90659c0458ae82691ef1e9ad342b
Andre Ramos Ribeiro <andre.ribeiro55@fatec.sp.gov.br>
2023-03-31 23:44:56 -0300

    bd zerado

--- Backend/src/migrations/1680316787041-default.ts
+++ Backend/src/migrations/1680316787041-default.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class default1679781026655 implements MigrationInterface {
-    name = 'default1679781026655'
+export class default1680316787041 implements MigrationInterface {
+    name = 'default1680316787041'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "ticket" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "title" varchar NOT NULL, "type" varchar NOT NULL, "description" varchar NOT NULL, "status" varchar NOT NULL)`);
