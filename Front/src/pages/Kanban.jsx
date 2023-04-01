f0deedffce84b1a37981c347d41c1d511fdc26c6
gabriel <gabrielcoutinho2011@gmail.com>
2023-04-01 00:37:03 -0300

    Filtro de busca no kanban

--- Front/src/pages/Kanban.jsx
+++ Front/src/pages/Kanban.jsx
@@ -8,6 +8,7 @@ export default function Kanban() {
   const [showModal, setShowModal] = React.useState(false);
   const [data, setData] = useState([])
   const [ticket, setTicket] = useState({ title: '', description: ``, classification: '' })
+  const [searchTerm, setSearchTerm] = useState('');
   function getData() {
     fetch("/ticket/getKanbanItem", {
       method: 'GET',
@@ -18,6 +19,9 @@ export default function Kanban() {
       var tickets = []
       console.log(data)
       data.forEach(element => {
+        if (
+            element.title.toLowerCase().includes(searchTerm.toLowerCase()) 
+          ){
         tickets.push({
           Id: element.id,
           Title: element.title,
@@ -34,7 +38,7 @@ export default function Kanban() {
 
         })
 
-
+      }
       });
       console.log(tickets)
       setData(tickets)
@@ -103,12 +107,23 @@ export default function Kanban() {
   }
   useEffect(() => {
     getData()
-  }, [])
+  }, [searchTerm])
 
   return (
     <>
       <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
         <Header category="App" title="Kanban" />
+        <div className="block relative">
+                <span className="h-full absolute inset-y-0 left-0 flex items-center pl-2">
+                    <svg viewBox="0 0 24 24" className="h-4 w-4 fill-current text-gray-500">
+                        <path
+                            d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
+                        </path>
+                    </svg>
+                </span>
+                <input placeholder="Search" onChange={(e) => setSearchTerm(e.target.value)}
+                    className="appearance-none rounded-full  border border-gray-400 border-b block pl-8 pr-6 py-2 w-44 bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+            </div>
         <KanbanComponent
           id="kanban"
           keyField="Status"
