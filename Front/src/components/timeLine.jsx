d111bbf9a5a51d248f8fd23cbe59cc299e71cbfd
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-05-02 18:08:49 -0300

    Diferenciação do histórico de chamados

--- Front/src/components/timeLine.jsx
+++ Front/src/components/timeLine.jsx
@@ -1,17 +1,13 @@
-const TimeLine = ({ data, titulo, autor, descricao }) => {
-
+const TimeLine = (props) => {
     return (
-
-        <ol class="relative border-l border-gray-200 dark:border-gray-700">
-            <li class="mb-10 ml-4">
-                <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{data} feito por: {autor}</time>
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{titulo}</h3>
-                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{descricao}</p>
-            </li>
-        </ol>
-
+      <li className={`mb-10 ml-4 ${props.ultimoItem ? "border-l-0" : "border-l border-gray-200 dark:border-gray-700"}`}>
+        <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+        <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{props.data} feito por: {props.usu}</time>
+        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{props.titulo}</h3>
+        <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{props.descricao}</p>
+      </li>
     );
-}
-
-export default TimeLine;
+  };
+  
+  export default TimeLine;
+  
--- Front/src/pages/Historic/historic.jsx
+++ Front/src/pages/Historic/historic.jsx
@@ -1,30 +1,65 @@
 import { Header } from "../../components";
+import TimeLine from "../../components/timeLine";
+
 
 const Historic = () => {
     const tiquete = [
-        { data: '12/12/12', titulo: "asd1", autor: "ele mesmo1", descricao: "asd1" },
-        { data: '13/12/12', titulo: "asd2", autor: "ele mesmo2", descricao: "asd2" },
-        { data: '14/12/12', titulo: "asd3", autor: "ele mesmo3", descricao: "asd3" },
+        { data: '12/12/12', tipo: "Criado", nomeUsu: "gabreu", idAcao: 1 },
+        { data: '13/12/12', tipo: "Aprovado risco", nomeUsu: "bruna", idAcao: 2 },
+        { data: '13/12/12', tipo: "Aprovado impacto", nomeUsu: "bruna", idAcao: 3 },
+        { data: '14/12/12', tipo: "Aprovado custo", nomeUsu: "marcelo", idAcao: 4 },
+        { data: '14/12/12', tipo: "Arquivado", nomeUsu: "emenerton", idAcao: 5 },
+        { data: '14/12/12', tipo: "Atualizado", nomeUsu: "emenerton", idAcao: 6 },
 
-    ]
+    ];
+
+    const cor = idAcao => {
+        switch (idAcao) {
+            case 1:
+                // Criado
+                return "bg-blue-500";
+            case 2:
+                // Aprovado risco
+                return "bg-green-500";
+            case 3:
+                // Aprovado impacto
+                return "bg-green-600"
+            case 4:
+                // Aprovado custo
+                return "bg-green-700";
+            case 5:
+                // Arquivado
+                return "bg-red-500";
+            case 6:
+                // Atualizado
+                return "bg-blue-500"
+            default:
+                // Caso tenha algum id que nao bate
+                return "bg-gray-500";
+        }
+    };
 
     return (
-
-        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-            <Header category="Page" title="Historic" />
-            <ol class="relative border-l border-gray-200 dark:border-gray-700">
-                {tiquete.map((tiq) =>
-                    <li class="mb-10 ml-4">
-                        <div class="absolute w-3 h-3 bg-gray-400 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-                        <time class="mb-1 text-sm font-semibold leading-none text-gray-659 dark:text-gray-659">{tiq.data} feito por: {tiq.autor}</time>
-                        <h3 class="text-lg font-bold text-gray-900 dark:text-gray-900">{tiq.titulo}</h3>
-                        <p class="mb-4 text-base font-semibold text-gray-500 dark:text-gray-400">{tiq.descricao}</p>
-
-                    </li>
-                )}
-            </ol>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div className="w-760 m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                <Header category="Page" title="Historic" />
+                <ol class="relative border-l border-gray-400">
+                    {tiquete.map((tiq) => (
+                        <li class="mb-10 ml-4">
+                            <div
+                                class={`${cor(tiq.idAcao)} absolute w-3 h-3 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-600`}
+                            ></div>
+                            <time class="mb-1 text-sm font-semibold leading-none text-gray-500">{tiq.data} </time>
+                            <h3 class="text-lg font-bold text-gray-900 dark:text-gray-900">{tiq.tipo}</h3>
+                            <p class="mb-4 text-base font-semibold text-gray-600">Realizado por {tiq.nomeUsu}</p>
+                        </li>
+                    ))}
+                </ol>
+            </div>
         </div>
+
+        
     );
-}
+};
 
 export default Historic;
