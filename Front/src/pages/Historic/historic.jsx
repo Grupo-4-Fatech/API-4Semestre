47066407b8c658931f8381a9f21fb988bdc1e5aa
Bruna <brunadias_30@hotmail.com>
2023-05-10 19:33:24 -0300

    coloquei titulo do ticket na pag de historico

--- Front/src/pages/Historic/historic.jsx
+++ Front/src/pages/Historic/historic.jsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 const Historic = () => {
     const { id } = useParams();
     const [data, setData] = useState([])
+    const [title, setTitle] = useState("")
     function getData() {
         if (id) {
             fetch("/ticket/getLog/" + id, {
@@ -15,14 +16,22 @@ const Historic = () => {
                 }
             }).then((resposta) => resposta.json()).then((res) => {
                 if (res != null) {
-                    console.log(res)
                     var logs = []
                     res.forEach(ele => {
                         logs.push({ data: ele.date, nomeUsu: ele.userName, idAcao: parseInt(ele.action) })
                     })
                     setData(logs)
                 }
-                console.log(data)
+            })
+            fetch("/ticket/get/" + id, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8'
+                }
+            }).then((resposta) => resposta.json()).then((data) => {
+                if (data != null) {
+                    setTitle(data.title);
+                }
             })
         }
     }
@@ -81,16 +90,16 @@ const Historic = () => {
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
             <div className="w-760 m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-                <Header category="Página" title="Histórico" />
-                <ol class="relative border-l border-gray-400">
-                    {data.map((tiq) => (
-                        <li class="mb-10 ml-4">
+                <Header category="Página" title={title} />
+                <ol className="relative border-l border-gray-400">
+                    {data.map((tiq,index) => (
+                        <li key={index} className="mb-10 ml-4">
                             <div
-                                class={`${cor(tiq.idAcao)} absolute w-3 h-3 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-600`}
+                                className={`${cor(tiq.idAcao)} absolute w-3 h-3 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-600`}
                             ></div>
-                            <time class="mb-1 text-sm font-semibold leading-none text-gray-500">{new Date(tiq.data).toLocaleDateString("en-US")} </time>
-                            <h3 class="text-lg font-bold text-gray-900 dark:text-gray-900">{acao(tiq.idAcao)}</h3>
-                            <p class="mb-4 text-base font-semibold text-gray-600">Realizado por {tiq.nomeUsu}</p>
+                            <time className="mb-1 text-sm font-semibold leading-none text-gray-500">{new Date(tiq.data).toLocaleDateString("en-US")} </time>
+                            <h3 className="text-lg font-bold text-gray-900 dark:text-gray-900">{acao(tiq.idAcao)}</h3>
+                            <p className="mb-4 text-base font-semibold text-gray-600">Realizado por {tiq.nomeUsu}</p>
                         </li>
                     ))}
                 </ol>
