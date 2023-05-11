7a623d4b9ec302e9a21de480d1db7b885fe539fd
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-05-11 08:37:19 -0300

    finalizando tradução updtade profile

--- Front/src/pages/User/UpdateProfile.jsx
+++ Front/src/pages/User/UpdateProfile.jsx
@@ -5,6 +5,8 @@ import { Header } from '../../components'
 import { useStateContext } from '../../contexts/ContextProvider'
 import { useNavigate } from 'react-router-dom';
 import { validador } from '../../utils/validador';
+import { useLanguage } from "../../contexts/contextLanguage";
+import tradutorUpdateProfile from '../../utils/tradutor/tradutorUpdateProfile';
 const Swal = require('sweetalert2')
 
 
@@ -17,6 +19,7 @@ export default function UpdateProfile() {
   const [email, setEmail] = useState("")
   const [gender, setGender] = useState("default")
   const [isChecked, setIsChecked] = useState(false)
+  const { language } = useLanguage();
 
   let location = useNavigate();
   function comeback() {
@@ -34,48 +37,48 @@ export default function UpdateProfile() {
     if (validador.estaVazio(name.value)) {
       Swal.fire({
         icon: 'error',
-        title: 'Falha na atualização do usuário!',
-        text: 'Por favor escreva um nome.',
+        title: tradutorUpdateProfile[language].errorTitle,
+        text: tradutorUpdateProfile[language].errorNameVazio,
       })
       return
     }
     if (validador.tamanhoTexto(name.value)) {
       Swal.fire({
         icon: 'error',
-        title: 'Falha na atualização do usuário!',
-        text: 'O tamanho do nome é muito grande.',
+        title: tradutorUpdateProfile[language].errorTitle,
+        text: tradutorUpdateProfile[language].errorNameTamanho,
       })
       return
     }
     if (validador.estaVazio(email.value)) {
       Swal.fire({
         icon: 'error',
-        title: 'Falha na atualização do usuário!',
-        text: 'Por favor escreva um e-mail.',
+        title: tradutorUpdateProfile[language].errorTitle,
+        text: tradutorUpdateProfile[language].errorEmailVazio,
       })
       return
     }
     if (!validador.validarEmail(email.value)) {
       Swal.fire({
         icon: 'error',
-        title: 'Falha na atualização do usuário!',
-        text: 'O e-mail deve ter @ e .com.',
+        title: tradutorUpdateProfile[language].errorTitle,
+        text: tradutorUpdateProfile[language].errorValidarEmail,
       })
       return
     }
     if (validador.tamanhoTexto(email.value)) {
       Swal.fire({
         icon: 'error',
-        title: 'Falha na atualização do usuário!',
-        text: 'O tamanho do e-mail é muito grande.',
+        title: tradutorUpdateProfile[language].errorTitle,
+        text: tradutorUpdateProfile[language].errorTamanhoEmail,
       })
       return
     }
     if (validador.selectEstaDefault(gender)) {
       Swal.fire({
         icon: 'error',
-        title: 'Falha na atualização do usuário!',
-        text: 'Selecione um gênero.',
+        title: tradutorUpdateProfile[language].errorTitle,
+        text: tradutorUpdateProfile[language].errorSelectDefaultGender,
       })
       return
     }
@@ -83,40 +86,40 @@ export default function UpdateProfile() {
       if (validador.estaVazio(oldPassword.value)) {
         Swal.fire({
           icon: 'error',
-          title: 'Falha na atualização do usuário!',
-          text: 'Por favor escreva a senha antiga.',
+          title: tradutorUpdateProfile[language].errorTitle,
+          text: tradutorUpdateProfile[language].errorSenhaVaziaVelha,
         })
         return
       }
       if (!validador.tamanhoSenha(oldPassword.value)) {
         Swal.fire({
           icon: 'error',
-          title: 'Falha na atualização do usuário!',
-          text: 'A senha deve ter entre 8 e 15 caracteres.',
+          title: tradutorUpdateProfile[language].errorTitle,
+          text: tradutorUpdateProfile[language].errorTamanoSenha,
         })
         return
       }
       if (validador.estaVazio(password.value)) {
         Swal.fire({
           icon: 'error',
-          title: 'Falha na atualização do usuário!',
-          text: 'Por favor escreva uma nova senha.',
+          title: tradutorUpdateProfile[language].errorTitle,
+          text: tradutorUpdateProfile[language].errorSenhaNova,
         })
         return
       }
       if (!validador.tamanhoSenha(password.value)) {
         Swal.fire({
           icon: 'error',
-          title: 'Falha na atualização do usuário!',
-          text: 'A senha deve ter entre 8 e 15 caracteres.',
+          title: tradutorUpdateProfile[language].errorTitle,
+          text: tradutorUpdateProfile[language].errorTamanoSenha,
         })
         return
       }
       if(!validador.senhaIgual(password.value,oldPassword.value)){
         Swal.fire({
           icon: 'error',
-          title: 'Falha na atualização do usuário!',
-          text: 'A nova senha não pode ser igual à senha antiga.',
+          title: tradutorUpdateProfile[language].errorTitle,
+          text: tradutorUpdateProfile[language].errorSenhasIguais,
         })
         return
       }
@@ -132,12 +135,13 @@ export default function UpdateProfile() {
       if (data.error) {
         Swal.fire({
           icon: 'error',
-          title: data.error,
+          title: tradutorUpdateProfile[language].errorTitle
+          // title: data.error,
         })
       } else {
         Swal.fire({
           icon: 'success',
-          title: 'Atualizado com sucesso.'
+          title: tradutorUpdateProfile[language].messageSucssefuly,
         }).then((result) => result.isConfirmed ? comeback() : '')
 
       }
@@ -165,31 +169,31 @@ export default function UpdateProfile() {
   useEffect(() => { getData() }, [])
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="Página" title="Atualizar usuário" />
-      <Campo text="Nome" id="name" placeholder="Nome" type={"text"} value={name} setValue={setName} />
-      <Campo text="Email" id="email" placeholder="Email" type={"text"} value={email} setValue={setEmail} />
-      <label className="text-lg font-bold dark:text-black " >Selecione um gênero</label>
+      <Header category={tradutorUpdateProfile[language].page} title={tradutorUpdateProfile[language].pageTitle} />
+      <Campo text={tradutorUpdateProfile[language].nomeTitle} id="name" placeholder={tradutorUpdateProfile[language].nomePlaceholder} type={"text"} value={name} setValue={setName} />
+      <Campo text={tradutorUpdateProfile[language].emailTitle} id="email" placeholder={tradutorUpdateProfile[language].emailPlaceholder} type={"text"} value={email} setValue={setEmail} />
+      <label className="text-lg font-bold dark:text-black " >{tradutorUpdateProfile[language].selectTitleGender}</label>
       <select id="gender" value={gender} onChange={(e) => setGender(e.target.value)} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500'>
-        <option value="default" disabled>Selecione uma opção:</option>
-        <option value="Male">Masculino</option>
-        <option value="Female">Feminino</option>
+        <option value="default" disabled>{tradutorUpdateProfile[language].selectTitleOp}</option>
+        <option value="Male">{tradutorUpdateProfile[language].selectTitleGenderOp2}</option>
+        <option value="Female">{tradutorUpdateProfile[language].selectTitleGenderOp3}</option>
       </select>
       <div className='mt-5'>
         <input onChange={() => setIsChecked(!isChecked)} type="checkbox" id="changePassword" />
-        <label className='ml-2'>Alterar a senha</label>
+        <label className='ml-2'>{tradutorUpdateProfile[language].alterarSenhaTitle}</label>
       </div>
 
       {
         isChecked ? (<>
-          <div className='my-6'> <Campo text="Senha Antiga" id="oldPassword" placeholder="*****" type={"password"} value={oldPassword} setValue={setOldPassword} /> </div>
-          <div className='my-6'> <Campo text="Nova Senha" id="password" placeholder="*****" type={"password"} value={password} setValue={setNewPassword} /> </div>
+          <div className='my-6'> <Campo text={tradutorUpdateProfile[language].senhaAntiga} id="oldPassword" placeholder={tradutorUpdateProfile[language].senhaPlaceholder} type={"password"} value={oldPassword} setValue={setOldPassword} /> </div>
+          <div className='my-6'> <Campo text={tradutorUpdateProfile[language].senhaNova} id="password" placeholder={tradutorUpdateProfile[language].senhaPlaceholder} type={"password"} value={password} setValue={setNewPassword} /> </div>
         </>
         ) : (<></>)
       }
 
       <div className="mt-5 mb-5 flex items-center justify-end" >
         <button style={{ backgroundColor: currentColor }} className="text-white font-bold py-2 px-4 rounded inline-flex items-center right-20" onClick={UpdateUser}>
-          <span className='pr-1'>Atualizar</span>
+          <span className='pr-1'>{tradutorUpdateProfile[language].buttonAtualizar}</span>
           <MdSend />
         </button>
       </div>
