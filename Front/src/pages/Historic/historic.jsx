48655fdb305450d1a3ce341855bd37b8e8aec21b
Bruna <brunadias_30@hotmail.com>
2023-05-11 22:47:16 -0300

    filtro historico e labels

--- Front/src/pages/Historic/historic.jsx
+++ Front/src/pages/Historic/historic.jsx
@@ -18,7 +18,7 @@ const Historic = () => {
                 if (res != null) {
                     var logs = []
                     res.forEach(ele => {
-                        logs.push({ data: ele.date, nomeUsu: ele.userName, idAcao: parseInt(ele.action) })
+                        logs.push({ data: ele.date, nomeUsu: ele.userName, idAcao: parseInt(ele.action), nota: ele.nota})
                     })
                     setData(logs)
                 }
@@ -56,6 +56,16 @@ const Historic = () => {
             case 6:
                 // Atualizado
                 return "bg-blue-500"
+            case 7:
+                // Avaliado risco
+                return "bg-green-500"
+            case 8:
+                // Avaliado impacto
+                return "bg-green-600"
+            case 9:
+                // Avaliado custo
+                return "bg-green-700"
+
             default:
                 // Caso tenha algum id que nao bate
                 return "bg-gray-500";
@@ -82,14 +92,14 @@ const Historic = () => {
                 // Atualizado
                 return "Atualizado"
             case 7:
-                    // Aprovado risco
-                    return "Avaliado risco";
+                // Avaliado risco
+                return "Avaliado risco";
             case 8:
-                    // Aprovado impacto
-                    return "Avaliado impacto"
+                // Avaliado impacto
+                return "Avaliado impacto"
             case 9:
-                    // Aprovado custo
-                    return "Avaliado custo";
+                // Avaliado custo
+                return "Avaliado custo";
             default:
                 // Caso tenha algum id que nao bate
                 return "bg-gray-500";
@@ -101,13 +111,14 @@ const Historic = () => {
             <div className="w-760 m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
                 <Header category="Página" title={title} />
                 <ol className="relative border-l border-gray-400">
-                    {data.map((tiq,index) => (
+                    {data.map((tiq, index) => (
                         <li key={index} className="mb-10 ml-4">
                             <div
                                 className={`${cor(tiq.idAcao)} absolute w-3 h-3 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-600`}
                             ></div>
                             <time className="mb-1 text-sm font-semibold leading-none text-gray-500">{new Date(tiq.data).toLocaleDateString("en-US")} </time>
                             <h3 className="text-lg font-bold text-gray-900 dark:text-gray-900">{acao(tiq.idAcao)}</h3>
+                            {tiq.idAcao>6 && <p className="text-base font-semibold text-gray-600">Nota: {tiq.nota}</p>}
                             <p className="mb-4 text-base font-semibold text-gray-600">Realizado por {tiq.nomeUsu}</p>
                         </li>
                     ))}
