16ca0437a19ef5801b84009e74abcc73b2c536ea
Gabriel Coutinho <gabrielcoutinho2011@gmail.com>
2023-05-12 15:00:25 -0300

    Finalizando tradução

--- Front/src/pages/Not_found/NotFound.jsx
+++ Front/src/pages/Not_found/NotFound.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { useStateContext } from '../../contexts/ContextProvider';
+import { useLanguage } from "../../contexts/contextLanguage";
+import tradutorNotFound from '../../utils/tradutor/notFound/tradutorNotFound';
+
 
 export default function NotFound() {
     const { setNotFound } = useStateContext();
+    const { language } = useLanguage();
+
     setNotFound(false);
     return (
         <div>
-            <h1 class="font-bold text-4xl dark:text-gray-200 text-gray-700 text-center m-20">Página não encontrada.</h1>
+            <h1 class="font-bold text-4xl dark:text-gray-200 text-gray-700 text-center m-20">{tradutorNotFound[language].notFound}</h1>
         </div>
     )
 }
---
+++ Front/src/utils/tradutor/notFound/tradutorNotFound.js
@@ -0,0 +1,14 @@
+const tradutorNotFound = {
+    pt: {
+        notFound: "Página não encontrada.",
+     
+    },
+    en: {
+        notFound: "Page not found.",
+     
+        
+    }
+};
+
+
+export default tradutorNotFound;
