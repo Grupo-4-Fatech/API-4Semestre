b855ee1cdfefc9966771c221b715ecd7ad939c04
Bruna <brunadias_30@hotmail.com>
2023-05-13 01:16:39 -0300

    arrumando select

--- Front/src/pages/Ticket/UpdateTicket.jsx
+++ Front/src/pages/Ticket/UpdateTicket.jsx
@@ -120,7 +120,7 @@ export default function UpdateTicket() {
             <Header category={translationsUpdateChamado[language].page} title={translationsUpdateChamado[language].pageTitle} />
             <Campo text={translationsUpdateChamado[language].ticketTitle} id="Titulo" placeholder={translationsUpdateChamado[language].ticketTitlePlaceholder} type={"text"} value={title} setValue={setTitle} />
             <label className="text-lg font-bold dark:text-black">{translationsUpdateChamado[language].selectName}</label>
-            <select id="select" onChange={(e) => setType(e.target.value)} defaultValue={type} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500'>
+            <select id="select" onChange={(e) => setType(e.target.value)} value={type} className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500'>
                 <option value="default" disabled>{translationsUpdateChamado[language].selectNameOption}:</option>
                 <option value="1" >Hotfix</option>
                 <option value="2" >Feature</option>
