35612a70844268ceb2a3c6f8c15300278a04390e
Bruna <brunadias_30@hotmail.com>
2023-05-23 21:36:16 -0300

    add chart header

--- Front/src/pages/Charts/Pie.jsx
+++ Front/src/pages/Charts/Pie.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import { pieChartData } from '../../data/dummy';
 import PieChart from '../../components/PieChart';
+import ChartsHeader from '../../components/ChartsHeader';
 
 
 const Pie = () => (
   <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-    {/* <ChartsHeader category="Pie" title="Project Cost Breakdown" /> */}
+    <ChartsHeader category="Pie" title="Quantidade de tickets por status" />
     <div className="w-full">
       <PieChart id="chart-pie" data={pieChartData} legendVisiblity height="full" />
     </div>
