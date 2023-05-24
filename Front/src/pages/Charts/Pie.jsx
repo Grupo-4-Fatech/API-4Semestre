cd5a451d66415188365bf2b6176a2e7c54971091
Bruna <brunadias_30@hotmail.com>
2023-05-24 01:05:54 -0300

    translate pie chart

--- Front/src/pages/Charts/Pie.jsx
+++ Front/src/pages/Charts/Pie.jsx
@@ -1,14 +1,10 @@
 import React, { useEffect, useState } from 'react';
-
-import { pieChartData } from '../../data/dummy';
 import PieChart from '../../components/PieChart';
 import ChartsHeader from '../../components/ChartsHeader';
-import { useStateContext } from '../../contexts/ContextProvider';
 import { useLanguage } from '../../contexts/contextLanguage';
 
 
 function Pie() {
-  const { currentMode, currentColor } = useStateContext();
   const [dataSource, setDataSource] = useState([]);
   const { language } = useLanguage();
 
@@ -21,16 +17,41 @@ function Pie() {
     })
       .then((response) => response.json())
       .then((data) => {
-        setDataSource(data);
+        const newData = alterarTextoEmIngles(data);
+        setDataSource(newData);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
   }
 
+  function alterarTextoEmIngles(data) {
+    if (language === 'en') {
+      const newData = data.map(item => {
+        switch (item.x) {
+          case "Em desenvolvimento":
+            return { ...item, x: "In progress" };
+          case "Concluído":
+            return { ...item, x: "Completed" };
+          case "Aprovado":
+            return { ...item, x: "Approved" };
+          case "Arquivado":
+            return { ...item, x: "Archived" };
+          case "Esperando aprovação":
+            return { ...item, x: "Waiting for approval" };
+          default:
+            return item;
+        }
+      });
+      return newData;
+    } else {
+      return data;
+    }
+  }
+
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [language]); // Observa a variável "language"
 
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
